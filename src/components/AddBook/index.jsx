c57ebba7b17b71688dc3c518a260f601513cf188
neongm <chura2013c@gmail.com>
2021-04-28 21:01:14 +0500

    restyled the 'add bock' page

--- src/components/AddBook/index.jsx
+++ src/components/AddBook/index.jsx
@@ -5,16 +5,19 @@ const AddBook = ( {onAdd}) => {
     // const [inputValue, setInputValue] = useState('');
 
     return (
-        <div className="add-book">
-            <span class="add-book__text">Добавить книгу</span>
-            {/* <input className="field" type="text" placeholder="Url адрес обложки книги" /> */}
-            <input className="field" type="text" placeholder="Название" />
-            <input className="field" type="text" placeholder="Год издания" />
-            <input className="field" type="text" placeholder="Автор" />
-            <input className="field" type="text" placeholder="Жанр" />
-            {/* <textarea className="field" type="text" placeholder="Описание" /> */}
-            <button className="button">Добавить</button>
-
+        <div className="container_add-book">
+            <div className="add-book">
+                <span className="add-book__text">Добавить книгу</span>
+                <br/>
+                {/* <input className="field" type="text" placeholder="Url адрес обложки книги" /> */}
+                <input className="field" type="text" placeholder="Название" />
+                <input className="field" type="text" placeholder="Год издания" />
+                <input className="field" type="text" placeholder="Автор" />
+                <input className="field" type="text" placeholder="Жанр" />
+                {/* <textarea className="field" type="text" placeholder="Описание" /> */}
+                <button className="button">Добавить</button>
+                <br/>
+            </div>
         </div>
     )
 }
