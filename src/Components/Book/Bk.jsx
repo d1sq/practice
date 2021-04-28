95ba413b99261441ca87b6250664b036c66c0742
d1sq <delord001@gmail.com>
2021-04-28 23:07:20 +0500

    need to add remove button

--- src/Components/Book/Bk.jsx
+++ src/Components/Book/Bk.jsx
@@ -1,11 +1,12 @@
 import './Book.scss';
 
-const Bk = ({ params }) => {
+const Bk = ({ params, onRemove }) => {
     return (
         <div className="book_cont">
             <img className="imgH" src="https://i.imgur.com/zoZvnTX.jpeg" alt="img"/>
             <div className="">{params.name}</div>
             <div className="">{params.year}</div>
+            <button onRemove={params.id}>X</button>
         </div>
     );
   };
