cdf1bc43a2128e9138801b03823207e61163795e
neongm <chura2013c@gmail.com>
2021-04-29 08:06:10 +0500

    fixed container issue on addBook page

--- src/components/AddBook/index.jsx
+++ src/components/AddBook/index.jsx
@@ -29,41 +29,43 @@ const AddBook = ({ onAdd }) => {
     setGenre("");
   };
   return (
-    <div className="add-book">
-      <span className="add-book__text">Добавить книгу</span>
-      {/* <input className="field" type="text" placeholder="Url адрес обложки книги" /> */}
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className="field"
-        type="text"
-        placeholder="Название"
-      />
-      <input
-        value={year}
-        onChange={(e) => setYear(e.target.value)}
-        className="field"
-        type="text"
-        placeholder="Год издания"
-      />
-      <input
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-        className="field"
-        type="text"
-        placeholder="Автор"
-      />
-      <input
-        value={genre}
-        onChange={(e) => setGenre(e.target.value)}
-        className="field"
-        type="text"
-        placeholder="Жанр"
-      />
-      {/* <textarea className="field" type="text" placeholder="Описание" /> */}
-      <button onClick={addBook} className="button">
-        Добавить
-      </button>
+    <div className="container_add-book">
+      <div className="add-book">
+        <span className="add-book__text">Добавить книгу</span>
+        {/* <input className="field" type="text" placeholder="Url адрес обложки книги" /> */}
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="field"
+          type="text"
+          placeholder="Название"
+        />
+        <input
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+          className="field"
+          type="text"
+          placeholder="Год издания"
+        />
+        <input
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          className="field"
+          type="text"
+          placeholder="Автор"
+        />
+        <input
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+          className="field"
+          type="text"
+          placeholder="Жанр"
+        />
+        {/* <textarea className="field" type="text" placeholder="Описание" /> */}
+        <button onClick={addBook} className="button">
+          Добавить
+        </button>
+      </div>
     </div>
   );
 };
