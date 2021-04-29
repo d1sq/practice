edc343a442c032ed86e08122843c85676b1b5cab
neongm <chura2013c@gmail.com>
2021-04-29 11:12:29 +0500

    normalized the files

--- src/Components/Book/index.jsx
+++ src/Components/Book/index.jsx
@@ -14,15 +14,13 @@ const Book = ({ items, onRemove }) => {
   return (
     <div className="books_container">
       {items.map((item) => (
-        <div key={item.id} className="book_cont">
-          <img
-            className="imgH"
-            src="https://i.imgur.com/zoZvnTX.jpeg"
-            alt="img"
-          />
-          <div className="">{item.name}</div>
-          <div className="">{item.year}</div>
-          <div onClick={() => removeBook(item)}>Delete book</div>
+        <div key={item.id} class="book_container">
+            <img src="https://sun9-52.userapi.com/impg/dTYRUVe3JtBcG-UNxcW-BoHEiMoVf18mupzrXg/wA5qfwrYSNM.jpg?size=436x604&quality=96&sign=249b0f97a32f81acd6b386a8bbb8f0c0&type=album" alt="img"/>
+            <button onClick={() => removeBook(item)} class="delete_button">X</button>
+          <div class="book__content_container">
+              <div class="book_name">{item.name}</div>
+              <div class="book_year">{item.year}</div>
+          </div>
         </div>
       ))}
     </div>
