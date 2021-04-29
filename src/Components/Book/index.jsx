a44ff2a6567799469e005d15ab4271088ea7ea02
d1sq <delord001@gmail.com>
2021-04-29 23:20:25 +0500

    restored to main pc

--- src/Components/Book/index.jsx
+++ src/Components/Book/index.jsx
@@ -14,12 +14,12 @@ const Book = ({ items, onRemove }) => {
   return (
     <div className="books_container">
       {items.map((item) => (
-        <div key={item.id} className="book_container">
-            <img className="imgH"src="https://sun9-52.userapi.com/impg/dTYRUVe3JtBcG-UNxcW-BoHEiMoVf18mupzrXg/wA5qfwrYSNM.jpg?size=436x604&quality=96&sign=249b0f97a32f81acd6b386a8bbb8f0c0&type=album" alt="img"/>
-            <button onClick={() => removeBook(item)} className="delete_button">X</button>
-          <div className="book__content_container">
-              <div className="book_name">{item.name}</div>
-              <div className="book_year">{item.year}</div>
+        <div key={item.id} class="book_container">
+            <img src="https://sun9-52.userapi.com/impg/dTYRUVe3JtBcG-UNxcW-BoHEiMoVf18mupzrXg/wA5qfwrYSNM.jpg?size=436x604&quality=96&sign=249b0f97a32f81acd6b386a8bbb8f0c0&type=album" alt="img"/>
+            <button onClick={() => removeBook(item)} class="delete_button">X</button>
+          <div class="book__content_container">
+              <div class="book_name">{item.name}</div>
+              <div class="book_year">{item.year}</div>
           </div>
         </div>
       ))}
