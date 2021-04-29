d4f837c26a3bfb2e3e1330cd843504b7b104127a
neongm <chura2013c@gmail.com>
2021-04-29 08:40:57 +0500

    normalized all the line endings

---
+++ src/Components/MainPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main-page.scss";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <h2>Практика weeb-разработки</h2>
+      <img src="https://i.imgur.com/DKpuMB1.gif" alt="foxGif"></img>
+      <span>authors: Dd, neongm</span>
+    </div>
+  );
+};
+
+export default MainPage;
