436852d91817a393758946d851c5205c39a648f1
neongm <chura2013c@gmail.com>
2021-04-30 13:01:48 +0500

    proper container on the main page, header-height in variables.scss

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -3,10 +3,12 @@ import "./Main-page.scss";
 
 const MainPage = () => {
   return (
-    <div className="main-page">
-      <h2>Практика weeb-разработки</h2>
-      <img src="https://i.imgur.com/DKpuMB1.gif" alt="foxGif"></img>
-      <span>authors: Dd, neongm</span>
+    <div className="main-page_container">
+      <div className="main-page">
+        <h2>Практика weeb-разработки</h2>
+        <img src="https://i.imgur.com/DKpuMB1.gif" alt="foxGif"></img>
+        <span>authors: Dd, neongm</span>
+      </div>
     </div>
   );
 };
